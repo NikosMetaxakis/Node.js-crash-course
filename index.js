51927c740ca9cb9e1ec1d1d6d80be8e0b19cd787
Nikos Metaxakis <metaxakisn@gmail.com>
2020-11-11 13:55:54 +0200

    serving pages exapmple start

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const Logger = require("./logger");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+    //console.log(req.url); //shows the current url
+    if(req.url === "/") {
+        fs.readFile(path.join(__dirname, "public", "index.html"), 
+        (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content);
+        });    
+    }
+    if(req.url === "/api/users") {
+        const users = [
+            { name: "bob", age: 40 },
+            { name: "Jonhy", age: 33 }
+        ];
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(users));
+    }
+});
 
-logger.on("message", (data) => console.log(
-    "Called Listener: ", data
-));
+const PORT = process.env.PORT || 5000;
 
-logger.log("hellow");
-
-/*on line 5 logger.on("message", ...) -> "message" is the event 
-that .log fires by default, so when you call
-logger.log("lalala"), this.emit("message", ...) is utilized
-and it displays the object that is 2nd argument in this.emit
-*/
+server.listen(PORT, () => console.log(
+    `Server running on port: ${PORT}`));
--- logger.js
+++ logger.js
@@ -11,4 +11,22 @@ class Logger extends EventEmitter {
     }
 }
 
-module.exports = Logger;
+//paste this in index.js
+/* module.exports = Logger;
+
+const Logger = require("./logger");
+
+const logger = new Logger();
+
+logger.on("message", (data) => console.log(
+    "Called Listener: ", data
+));
+
+logger.log("hellow");
+ */
+
+/*logger.on("message", ...) -> "message" is the event 
+that .log fires by default, so when you call
+logger.log("lalala"), this.emit("message", ...) is utilized
+and it displays the object that is 2nd argument in this.emit
+*/
